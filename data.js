e7b5d5a8d1d56358632fbf835621f3665fb3febe
naglis mockevicius <48673743+naglismoc@users.noreply.github.com>
2021-05-08 10:30:27 +0300

    pokemonData

---
+++ data.js
@@ -0,0 +1,18 @@
+const data = [0,2,3];
+
+const dataObj ={
+    species:"bird",
+    pet:"parrot"
+};
+
+const whoman = {
+    name:'Naglis',
+    surname:'Mockevicius',
+    age:22, 
+    runDates:[1,5,8],
+    pet:{
+        name:"ciucis",
+        species:"canis",
+        age:3
+    }
+}
